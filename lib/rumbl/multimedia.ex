8505bb72fd169a937c8240c3f7024055f00dfc96
Kristopher Manceaux <krismanceaux@hotmail.com>
2020-03-24 23:24:42 -0500

    Added a function to the Multimedia context to insert categories into the DB then called that function in our seed.exs script. Used the mix run command to run the seed script

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -8,6 +8,7 @@ defmodule Rumbl.Multimedia do
 
   alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts
+  alias Rumbl.Multimedia.Category
 
   @doc """
   Returns the list of videos.
@@ -118,5 +119,9 @@ defmodule Rumbl.Multimedia do
 
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
     from(v in query, where: v.user_id == ^user_id)
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
   end
 end
